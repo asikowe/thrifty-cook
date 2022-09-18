42713438b64c5249888071a94bd447fb1ee7a3b6
bodl3178 <joanna.bek@bodleian.ox.ac.uk>
2022-09-18 10:05:34 +0200

    Fix screen cutting off on search page

--- src/pages/SearchPage.js
+++ src/pages/SearchPage.js
@@ -108,7 +108,7 @@ export default function Searching() {
                 </View>
             }
             {!! response &&
-                <View>
+                <View style={Style.container}>
                         <ScrollView contentContainerStyle={Style.containerRecipeCards} showsHorizontalScrollIndicator={false}>
                         {response?.results.map((result) => (
                             <RecipeCard title={result.title} 
