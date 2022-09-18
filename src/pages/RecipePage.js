ce3ad67b4e92376a879bab1c6d752231671a7773
bodl3178 <joanna.bek@bodleian.ox.ac.uk>
2022-09-18 10:43:44 +0200

    Fix 'each child should have unique key' error

--- src/pages/RecipePage.js
+++ src/pages/RecipePage.js
@@ -10,15 +10,15 @@ export default function Recipe({ route }) {
             <Text style={Style.heading}>{route.params?.paramKey.title}</Text>
             <Text style={Style.subtitle}>Ready in: {route.params?.paramKey.readyInMinutes} minutes. Servings: {route.params?.paramKey.servings}</Text>
             <Text style={Style.subtitle}>INGREDIENTS:</Text>
-                <View style={Style.containerInstructions}>
-                    {route.params?.paramKey.extendedIngredients.map((item) => ( 
-                        <Text style={Style.textInstructions} key={item.id}>{item.original}</Text>
+                <View style={Style.containerInstructions} key={route.params?.paramKey.id}>
+                    {route.params?.paramKey.extendedIngredients.map((item, i) => ( 
+                        <Text style={Style.textInstructions} key={i}>{item.original}</Text>
                     ))}
                 </View>
             <Text style={Style.subtitle}>COOKING INSTRUCTIONS:</Text>
-                <View style={Style.containerInstructions}>
-                    {route.params?.paramKey.analyzedInstructions[0].steps.map((item) => (
-                        <Text style={Style.textInstructions} key={item.step}>{item.number}. {item.step}</Text>
+                <View style={Style.containerInstructions} key={route.params?.paramKey.title}>
+                    {route.params?.paramKey.analyzedInstructions[0].steps.map((item, i) => (
+                        <Text style={Style.textInstructions} key={i}>{item.number}. {item.step}</Text>
                     ))}
                 </View>
         </ScrollView>
