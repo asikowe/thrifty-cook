464d10598dfeb5e7894845681414f2d5a0a3b6d2
anna-lewandowska <bakatenko@gmail.com>
2022-09-20 17:29:33 +0200

    Minor spelling amendments

--- src/components/IngredientsButton.js
+++ src/components/IngredientsButton.js
@@ -1,4 +1,4 @@
-import { TouchableOpacity } from "react-native";
+import { TouchableOpacity } from 'react-native';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 
 export default function IngredientsButton(props) {
